e369245438241bb1e5f69c7c01aa82ed2bb8cbd7
Tatev <tatev.barseghyan1996@gmail.com>
2019-03-31 02:36:41 -0700

    adding remove function

--- src/binary_tree.hpp
+++ src/binary_tree.hpp
@@ -17,31 +17,59 @@ class btree
 {
     private:
         node<T> *m_root;
+        //help function to insert node
         void insert_help(T data, node<T>* curr_root);
-        bool find_help(T item, node<T>* curr_root);
+
+        //help funcion to find given item (without recursion)
+        node<T>* find_help(T item, node<T>* curr_root);
+
+        //help funcion to find given item (recursion)
+        bool search_help(T item, node<T>* curr_root);
+
+        //this function uded in destructor
         void delete_node(node<T>* root);
+
+        //help function to print tree postorder
         void print_postorder_help(node <T>* curr_root);
+
+        //help function to print tree preorder
         void print_preorder_help(node <T>* curr_root);
+
+        //help function to print tree inorder
         void print_inorder_help(node <T>* curr_root);
-        node<T>* remove_help(node <T>* curr_root, T item);
-        node<T>* find_min(node<T>* curr_root);
+
+        //help function to calculate height of tree
         int height_help(node<T> * curr_root);
+
+        //print tree (given level)
         void print_given_level(node<T>* curr_root, int level);
-   //     void print_level_order1(node<T>* curr_root);
+
+        //print tree (given level) used queue
+        void print_level_order1(node<T>* curr_root);
+
+        //help function to delete node when it is a root of tree
+        void remove_root();
+
+        //help function to delete node with given item
+        void remove_help(node<T> * curr,T item);
+
+        //this function give node with minimum value
+        node<T> * find_min(node<T>* curr_root);
+
     public:
         btree();
         ~btree();
-
         void insert(T item);
-        bool find(T item);
+        node<T>* find(T item);
+        bool search(T item);
         void print();
         void print_postorder();
         void print_preorder();
         void print_inorder();
-        node<T>* remove(T item);
         int height();
         void print_level_order();
-     //   void print_level_order1();;
+        void print_level_order1();
+        void remove(T item);
 };
 
 #endif
@@ -107,44 +135,65 @@ void btree<T>::insert(T data)
     }
 }
 
-//template <typename T>
-//bool btree<T>::find_help(T data, node<T> * curr_root)
-//{
-//    if (curr_root != NULL) {
-//        if (data == curr_root->data) {
-//            return true;
-//        }
-//        if (data < curr_root->data) {
-//            return find_help(data, curr_root->left);
-//        } else {
-//            return find_help(data, curr_root->right);
-//        }
-//    }
-//    else return false;
-//}
-
     template <typename T>
-bool btree<T>::find_help(T data, node<T> * curr_root)
+bool btree<T>::search_help(T data, node<T> * curr_root)
 {
-    while (curr_root != NULL) {
+    if (curr_root != NULL) {
+        if (data == curr_root->data) {
+            return true;
+        }
         if (data < curr_root->data) {
-            curr_root = curr_root->left;
-        } else
-            if (data > curr_root->data) {
-                curr_root = curr_root->right;
-            }
-            else {
-                return true;
-            }
+            return search_help(data, curr_root->left);
+        } else {
+            return search_help(data, curr_root->right);
+        }
     }
-    return false;
+    else return false;
 }
 
     template <typename T>
-bool btree<T>::find(T data)
+bool btree<T>::search(T data)
 {
-    find_help(data,m_root);
+    return search_help(data,m_root);
 }
+
+    template <typename T>
+node<T>* btree<T>::find_help(T data, node<T> * curr_root)
+{
+    node<T>* tmp = NULL;
+    if (curr_root->right != NULL || curr_root->left != NULL) {
+        while (curr_root != NULL) {
+            if (data < curr_root->data) {
+                tmp = curr_root;
+                curr_root = curr_root->left;
+            } else
+                if (data > curr_root->data) {
+                    tmp = curr_root;
+                    curr_root = curr_root->right;
+                }
+                else {
+                    return tmp ;
+                }
+        }
+    }
+    return tmp;
+}
+
+    template <typename T>
+node<T>* btree<T>::find(T data)
+{
+    return find_help(data,m_root);
+}
+
+    template <typename T>
+node<T>* btree<T>::find_min(node<T>* curr_root)
+{
+    while (curr_root->left != NULL) {
+        curr_root = curr_root->left;
+    }
+    return curr_root;
+}
+
 
     template <typename T>
 void btree<T>::print_inorder_help(node <T> * curr_root)
@@ -163,6 +212,7 @@ void btree<T>::print_inorder()
     print_inorder_help(m_root);
     std::cout << std::endl;
 }
+
     template <typename T>
 void btree<T>::print_preorder_help(node <T> * curr_root)
 {
@@ -180,6 +230,7 @@ void btree<T>::print_preorder()
     print_preorder_help(m_root);
     std::cout << std::endl;
 }
+
     template <typename T>
 void btree<T>::print_postorder_help(node <T> * curr_root)
 {
@@ -191,7 +242,6 @@ void btree<T>::print_postorder_help(node <T> * curr_root)
     std::cout <<" " << curr_root->data;
 }
 
-
     template <typename T>
 void btree<T>::print_postorder()
 {
@@ -200,73 +250,27 @@ void btree<T>::print_postorder()
 }
 
     template <typename T>
-node<T> * btree<T>::remove_help(node <T> * curr_root, T item)
-{
-    if (curr_root == NULL) {
-        return NULL;
-    }
-    else if (item < curr_root->data) {
-        curr_root->left = remove_help(curr_root->left, item);
-    } else if (item > curr_root->data) {
-        curr_root->right = remove_help(curr_root->right, item);
-    } else if (curr_root->data == item) {
-        if (curr_root->left == NULL && curr_root->right == NULL) {
-            delete curr_root;
-            curr_root = NULL;
-        } else if (curr_root->left == NULL) {
-            node<T>* tmp = curr_root;
-            curr_root = curr_root->right;
-            delete tmp;
-        } else if (curr_root->right == NULL) {
-            node<T>* tmp = curr_root;
-            curr_root = curr_root ->left;
-            delete tmp;
-        } else {
-            node<T>* tmp = find_min(curr_root->right);
-            curr_root->data = tmp->data;
-            curr_root->right = remove_help(curr_root->right,tmp->data);
-        }
-    }
-    return curr_root;
-}
-
-    template <typename T>
-node<T>* btree<T>::remove(T item)
-{
-    return remove_help(m_root, item);
-}
-
-    template <typename T>
-node<T>* btree<T>::find_min(node<T>* curr_root)
-{
-    while (curr_root->left != NULL) {
-        curr_root = curr_root->left;
-    }
-    return curr_root;
-}
-
-template <typename T>
 int btree<T>::height_help(node<T> * curr_root)
 {
     int h = 0;
     if (curr_root !=  NULL) {
-       h++;
-       if (height_help(curr_root->left) > height_help(curr_root->right)) {
-        h += height_help(curr_root->left);
-       } else {
-        h += height_help(curr_root->right);
-       }
+        h++;
+        if (height_help(curr_root->left) > height_help(curr_root->right)) {
+            h += height_help(curr_root->left);
+        } else {
+            h += height_help(curr_root->right);
+        }
     }
     return h;
 }
 
-template <typename T>
+    template <typename T>
 int btree<T>::height()
 {
     return height_help(m_root);
 }
 
-template <typename T>
+    template <typename T>
 void btree<T>::print_given_level(node<T>* curr_root, int level)
 {
     if (curr_root == NULL) {
@@ -280,7 +284,7 @@ void btree<T>::print_given_level(node<T>* curr_root, int level)
     }
 }
 
-template <typename T>
+    template <typename T>
 void btree<T>::print_level_order()
 {
     for (int i = 1; i <= height(); ++i) {
@@ -289,13 +293,13 @@ void btree<T>::print_level_order()
     std::cout << std::endl;
 }
 
-/*template <typename T>
+    template <typename T>
 void btree<T>::print_level_order1(node<T> * curr_root)
 {
     if (curr_root == NULL) {
         return;
     }
-    std::queue<T> node * q;
+    std::queue<node <T> *> q;
     q.push(curr_root);
     while (!q.empty()) {
         node<T> * tmp = q.front();
@@ -307,13 +311,108 @@ void btree<T>::print_level_order1(node<T> * curr_root)
         if (tmp->right != NULL) {
             q.push(tmp->right);
         }
-        std::cout << std::endl;
     }
-}*/
+    std::cout << std::endl;
+}
 
-/*template <typename T>
+    template <typename T>
 void btree<T>::print_level_order1()
 {
     print_level_order1(m_root);
-}*/
+}
 
+    template <typename T>
+void btree<T>::remove_help(node<T> * curr, T item)
+{
+    node<T> * parent = find(item);
+    if (m_root == NULL) {
+        std::cout << "is empty";
+    } else if (m_root->data == item) {
+        remove_root();
+    }
+    else {
+        if (parent->left != NULL && parent->left->left == NULL && parent->left->right == NULL && parent->left->data == item) {
+            delete parent->left;
+            parent->left = NULL;
+        } else if (parent->right != NULL && parent->right->left == NULL && parent->right->right == NULL && parent->right->data == item) {
+            delete parent->right;
+            parent->right = NULL;
+        } else if (parent->right != NULL && parent->right->data == item && (parent->right->left == NULL || parent->right->right == NULL)) {
+            if (parent->right->left == NULL && parent->right->right != NULL) {
+                node<T> * tmp = parent->right;
+                parent->right = parent->right->right;
+                delete tmp;
+            } else if (parent->right->left != NULL && parent->right->right == NULL) {
+                node<T> * tmp = parent->right;
+                parent->right = parent->right->left;
+                delete tmp;
+            }
+        } else if (parent->left != NULL && parent->left->data == item && (parent->left->left == NULL || parent->left->right == NULL)) {
+            if (parent->left->left == NULL && parent->left->right != NULL) {
+                node<T> * tmp = parent->left;
+                parent->left = parent->left->right;
+                delete tmp;
+            } else if (parent->left->left != NULL && parent->left->right == NULL) {
+                node<T> * tmp = parent->left;
+                parent->left = parent->left->left;
+                delete tmp;
+            }
+        } else {
+            if (parent->right->data == item) {
+                node<T> * min = find_min(parent->right);
+                node<T> * parent1 = find(min->data);
+                parent->right->data = min->data;
+                if (min->right == NULL) {
+                    parent1->left = NULL;
+                } else {
+                    parent1->left = min->right;
+                }
+            } else {
+                node<T> * min = find_min(parent->left);
+                node<T> * parent1 = find(min->data);
+                parent->left->data = min->data;
+                if (min->right == NULL) {
+                    parent1->left = NULL;
+                } else {
+                    parent1->left = min->right;
+                }
+            }
+        }
+    }
+}
+
+    template <typename T>
+void btree<T>::remove_root()
+{
+    if (m_root == NULL) {
+        std::cout << "The tree is empty" << std::endl;
+    } else
+        if (m_root->right == NULL && m_root->left == NULL) {
+            node<T> * tmp = m_root;
+            m_root = NULL;
+            delete tmp;
+        } else  if (m_root->right == NULL && m_root->left != NULL) {
+            node<T> * tmp = m_root;
+            m_root = m_root->left;
+            delete tmp;
+        } else if (m_root->left == NULL && m_root->right != NULL) {
+            node<T> * tmp = m_root;
+            m_root = m_root->right;
+            delete tmp;
+        } else  {
+            node<T> * min = find_min(m_root->right);
+            node<T> * parent = find(min->data);
+            m_root->data = min->data;
+            if (min->right == NULL) {
+                parent->left = NULL;
+            } else {
+                parent->left = min->right;
+            }
+        }
+}
+
+    template <typename T>
+void btree<T>::remove(T item)
+{
+    remove_help(m_root,item);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,19 @@ int main()
 {
     btree <int> b;
     b.insert(10);
-    b.insert(5);
-    b.insert(20);
-    b.insert(22);
-    b.insert(3);
-    b.insert(15);
-    b.insert(27);
-    b.insert(7);
-    std::cout << "Postorder  : ";
+   b.insert(5);
+   b.insert(20);
+ b.insert(22);
+ b.insert(21);
+ b.insert(3);
+ b.insert(15);
+ b.insert(27);
+ b.insert(7);
+ b.insert(25);
+ b.insert(1);
+ b.insert(8);
+ b.insert(26);
+  std::cout << "Postorder  : ";
     b.print_postorder();
     std::cout << "Preorder   : ";
     b.print_preorder();
@@ -20,21 +25,23 @@ int main()
     b.print_inorder();
     std::cout << "Level order travers : ";
     b.print_level_order();
-   // std::cout << "Level order travers1 : ";
-    //b.print_level_order1();
-    if (b.find(22)) {
+    std::cout << "Level order travers1 : ";
+    b.print_level_order1();
+    if (b.search(22)) {
         std::cout << "This item have in tree " << std::endl;
     } else {
         std::cout << "This item have not in tree " << std::endl;
     }
-    if (b.find(55)) {
+    if (b.search(55)) {
         std::cout << "This item have in tree " << std::endl;
     } else {
         std::cout << "This item have not in tree " << std::endl;
     }
-    b.remove(20);
-    std::cout << "Inorder    : ";
-    b.print_inorder();
-    std::cout << "Tree height is  : " << b.height() << std::endl;
+   std::cout << "Inorder    : ";
+  b.print_inorder();
+    b.remove(22);
+   std::cout << "Inorder    : ";
+   b.print_inorder();
+  std::cout << "Tree height is  : " << b.height() << std::endl;
 }
 
